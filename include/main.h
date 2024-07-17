e87315669a476204b091cb84bf2a55f718bc65b7
Savo Bajic <bajic.savo@gmail.com>
2024-07-17 01:15:15 -0400

    Baseline I2C communication functions working

---
+++ include/main.h
@@ -0,0 +1,6 @@
+#ifndef PITOT_MAIN_H
+#define PITOT_MAIN_H
+
+void send_i2c_response();
+void send_float_i2c(float data);
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
 #include "nd005.hpp"
 #include <SPI.h>
+#include <Wire.h>
+#include "main.h"
+
+const uint8_t I2C_ADDRESS = 0x19;
+const uint32_t MAIN_SDA = 2;
+const uint32_t MAIN_SCL = 3;
+MbedI2C main_i2c(MAIN_SDA, MAIN_SCL);
 
 // Added "_USED" due to naming collision in library with just functional names
 static const pin_size_t MISO_USED = 4;
@@ -26,6 +33,9 @@ void setup() {
   while (!Serial) delay(10); // Wait until a PC is connected
 
   Serial.println("PEETOESTATIK CYSTDUMB\n");
+
+  main_i2c.begin(I2C_ADDRESS);
+  main_i2c.onRequest(send_i2c_response); // register event
 }
 
 int j = 0;
@@ -50,4 +60,27 @@ void loop() {
   Serial.print(windSpeedVector[2]);
   Serial.println("");
   delay(100);
+}
+
+void send_i2c_response() {
+  send_float_i2c(23.5);     // 0x41BC_0000
+  send_float_i2c(4983.976); // 0x459B_BFCF
+  send_float_i2c(-5675.05); // 0xC5B1_5866
+
+  /* NOTE:
+
+  Something about this function seems to cause instability if the full 12 bytes are not
+  read. The microcontroller seems to get stuck trying to transmit messing up the I2C bus.
+
+  Might make sense in the future to opt for shorter exchanges where only a single axis is 
+  commanded for at a time by the motherboard writing to this board first. Even if the bus 
+  gets more congested overall.
+  */
+}
+
+void send_float_i2c(float data) {
+  char * dataPointer = (char *) & data;
+
+  for (int i = 4; i > 0; i--) main_i2c.write(dataPointer[i-1]); // Most significant byte first
+  // for (int i = 0; i < 4; i++) main_i2c.write(dataPointer[i]); // Least significant byte first
 }
